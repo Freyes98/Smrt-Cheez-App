7958778a50b90634c3e30f8d56276ab9fdb8a574
GREST15 <paco_javer@hotmail.com>
2022-08-13 13:12:40 -0500

    Se añadió funcion para cargar imagen de sensor dependiendo del tipo

--- SmrtCheese/SensorValuesViewController.swift
+++ SmrtCheese/SensorValuesViewController.swift
@@ -26,10 +26,29 @@ class SensorValuesViewController: UIViewController {
         
         
         name_sensor.text = sensor?.nombreSensor
+        
+        if sensor?.tipo == "ultrasonico"{
+            sensor_img.image = UIImage(named: "Ultrasonico.png")
+        }
+        
+        else if sensor?.tipo == "temperatura"{
+            sensor_img.image = UIImage(named: "Temperatura.png")
+        }
+        else if sensor?.tipo == "flama"{
+            sensor_img.image = UIImage(named: "Flama.png")
+        }
+        else if sensor?.tipo == "humo"{
+            sensor_img.image = UIImage(named: "humo.png")
+        }
+        else if sensor?.tipo == "infrarojo"{
+            sensor_img.image = UIImage(named: "Infrarojo.png")
+        }
+        
         // Do any additional setup after loading the view.
         Api.shared.Sensor_lastValue(tkn: tkn, id_sensor: "62f2f560eb0eb706a86922c6"){(result) in
             switch result {
             case .success(let json):
+                
                 
                 self.value_sensor.text = "\(Int((json as! Value).value))"
                 //let tkn = (json as! ResponseProfile).email
