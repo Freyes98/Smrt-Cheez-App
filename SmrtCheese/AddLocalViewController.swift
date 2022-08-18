2be47f702a429bd581cc1db971074bdb197c18a7
GREST15 <paco_javer@hotmail.com>
2022-08-17 23:58:55 -0500

    Agregado horario en picker view

--- SmrtCheese/AddLocalViewController.swift
+++ SmrtCheese/AddLocalViewController.swift
@@ -14,21 +14,40 @@ class AddLocalViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var telefono_local: UITextField!
     @IBOutlet weak var name_local: UITextField!
     @IBOutlet weak var Btn_Añadir: UIButton!
-    @IBOutlet weak var horario_apertura: UIDatePicker!
+    //@IBOutlet weak var horario_apertura: UIDatePicker!
+    @IBOutlet weak var apertura_picker: UIPickerView!
+    var apertura: String?
+    var cierre: String?
     
-    @IBOutlet weak var horario_cierre: UIDatePicker!
+    @IBOutlet weak var Cierre_picker: UIPickerView!
+    //@IBOutlet weak var horario_cierre: UIDatePicker!
     var recibir_id : String?
-   
+    var horarios = ["1:00 AM","2:00 AM","3:00 AM","4:00 AM","5:00 AM","6:00 AM","7:00 AM","8:00 AM","9:00 AM","10:00 AM","11:00 AM","12:00 AM","1:00 PM","2:00 PM","3:00 PM","4:00 PM","5:00 PM","6:00 PM","7:00 PM","8:00 PM","9:00 PM","10:00 PM","11:00 PM","12:00 PM"]
+    //var horario : String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         //horario_apertura.Format = DateTimePickerFormat.Custom
         telefono_local.delegate = self
-   
-     
 
+        apertura_picker.delegate = self
+        apertura_picker.dataSource = self
+        
+        Cierre_picker.delegate = self
+        Cierre_picker.dataSource = self
+        
         // Do any additional setup after loading the view.
+        
+        
+        
     }
+    
+ 
+
+      // Number of columns of data
+     
+    
+    
     //funcion para ingresar solo numeros
     func textField(_ _textField:UITextField,shouldChangeCharactersIn range: NSRange,replacementString string:
         String)-> Bool{
@@ -86,7 +105,7 @@ class AddLocalViewController: UIViewController, UITextFieldDelegate {
    
             
             
-            let horario = "De \(horario_apertura.date) A \(horario_cierre.date)"
+            let horario = "De \(apertura ?? "") A \(cierre ?? "")"
             
             let local = Local(nombre_queseria: nombre, telefono: telefono, direccion: direccion, horarios: horario, descripcion: descripcion,token: tkn)
             
@@ -124,14 +143,32 @@ class AddLocalViewController: UIViewController, UITextFieldDelegate {
         
     }
     
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension AddLocalViewController: UIPickerViewDelegate, UIPickerViewDataSource{
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
     }
-    */
-
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        horarios.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        if pickerView == apertura_picker {
+            apertura = horarios[row]
+            
+         } else if pickerView == Cierre_picker{
+             cierre = horarios[row]
+         }
+        
+        return horarios[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+    }
+    
 }
